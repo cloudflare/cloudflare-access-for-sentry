7d12f73caaaad743a1e8c906ad0dfdc217d17d5b
Felipe Nascimento <thrash.nation@gmail.com>
2020-01-08 15:52:50 +0000

    Fixing build script for sentry 10x

--- build-docker-10x.sh
+++ build-docker-10x.sh
@@ -4,6 +4,7 @@ set -e
 VERSION=`head VERSION`
 WHL_FILENAME=sentry_cloudflare_access_auth-$VERSION-py2-none-any.whl
 TARGET_WHL_FILENAME=sentry_cloudflare_access_auth-dev-py2-none-any.whl
+SENTRY_DC_PREFIX=sentry-docker-10x
 
 cd src 
 
@@ -11,17 +12,18 @@ python setup.py sdist bdist_wheel
 
 cd ..
 
-cp src/dist/$WHL_FILENAME sentry-docker/sentry/$TARGET_WHL_FILENAME
+cp src/dist/$WHL_FILENAME $SENTRY_DC_PREFIX/sentry/$TARGET_WHL_FILENAME
 
-cd sentry-docker/
+cd sentry-docker-10x/
 
-docker stop sentry-docker_cron_1 sentry-docker_web_1  sentry-docker_worker_1
+
+docker stop $SENTRY_DC_PREFIX"_cron_1" $SENTRY_DC_PREFIX"_web_1" $SENTRY_DC_PREFIX"_worker_1"
 docker rmi -f sentry-onpremise-local
 docker-compose build
 docker-compose up -d
 
 cd ..
 
-rm -v sentry-docker/sentry/$TARGET_WHL_FILENAME
+rm -v $SENTRY_DC_PREFIX/sentry/$TARGET_WHL_FILENAME
 
 echo "sentry-cloudflare-access-auth build finished!"
