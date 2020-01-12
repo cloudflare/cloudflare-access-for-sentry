ac390b3dae126d16d01be1506460acb2f2a714fe
Felipe Nascimento <thrash.nation@gmail.com>
2020-01-12 17:47:42 +0000

    adding wait as when selenium went too fast test would break

--- test/selenium_test/test.py
+++ test/selenium_test/test.py
@@ -1,7 +1,11 @@
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
+
+def std_wait_for_action():
+    time.sleep(2)
 
 class SentryPluginEndToEndTest(unittest.TestCase):
 
@@ -16,27 +20,29 @@ class SentryPluginEndToEndTest(unittest.TestCase):
         self.driver.get("http://securesentry/cdn-cgi/access/auth?email={0}".format(self.user_email))
 
     def test_login(self):
+        std_wait_for_action()
         driver = self.driver
         
         driver.get("http://securesentry/")
-
+        
         sidebar_dropdown = driver.find_element_by_css_selector('[data-test-id="sidebar-dropdown"]')
         assert self.user_email in sidebar_dropdown.text
 
 
     def test_logout(self):
+        std_wait_for_action()
         driver = self.driver
         
         driver.get("http://securesentry/")
-
+        
+        std_wait_for_action()
         sidebar_dropdown = driver.find_element_by_css_selector('[data-test-id="sidebar-dropdown"]')
         sidebar_dropdown.click()
-
+        
+        std_wait_for_action()
         signout_elm = driver.find_element_by_css_selector('[data-test-id="sidebarSignout"]')
         signout_elm.click()
-
-        print("000000000000000000000000000000000000000")
-        print(driver.page_source)
+                
         # Assertion on the mock server result
         assert "Logout succesful" in driver.page_source
 
